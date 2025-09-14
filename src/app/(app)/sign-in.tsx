fae8f71feec31334c7ef835ba15f79ba776b34e7
miswah <miswah.md@gmail.com>
2025-09-14 10:43:30 +0530

    (feat): Auth flows completed

--- src/app/(app)/sign-in.tsx
+++ src/app/(app)/sign-in.tsx
@@ -1,6 +1,6 @@
 import { useSignIn } from '@clerk/clerk-expo'
 import { Link, useRouter } from 'expo-router'
-import { KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Ionicons } from '@expo/vector-icons'
@@ -18,6 +18,13 @@ export default function Page() {
   // Handle the submission of the sign-in form
   const onSignInPress = async () => {
     if (!isLoaded) return
+
+    if(!emailAddress || !password) {
+      Alert.alert("Error", "Please fill in all the fields");
+      return;
+    }
+
+    setIsLoading(true);
 
     // Start the sign-in process using the email and password provided
     try {
@@ -40,6 +47,8 @@ export default function Page() {
       // See https://clerk.com/docs/custom-flows/error-handling
       // for more info on error handling
       console.error(JSON.stringify(err, null, 2))
+    } finally {
+      setIsLoading(false);
     }
   }
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,10 +2,8 @@ import "../global.css";
 import { Slot } from "expo-router";
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { ClerkProvider } from '@clerk/clerk-expo'
-import { verifyInstallation } from 'nativewind';
 
 export default function Layout() {
-  verifyInstallation()
   return (
       <ClerkProvider tokenCache={tokenCache}>
          <Slot />
